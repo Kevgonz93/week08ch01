a890796332dd7ffb6fb62a74c7491abcffbd8d01
Kev <kev@Air-de-Kevin.localdomain>
2024-04-24 09:25:10 +0200

    encrypt added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PetsModule } from './pets/pets.module';
 import { PrismaService } from './prisma/prisma.service';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [UsersModule, PetsModule],
+  imports: [UsersModule, PetsModule, CoreModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
